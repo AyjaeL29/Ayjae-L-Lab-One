ec364f99dbcc8c825c5f9dd5097b8fcbb5b72f4e
AyjaeL29 <alarson29@setoncatholic.org>
2026-09-17 21:43:54 +0000

    Second save

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print (5/5)
+name = input ("Hello, what is your name?")
+age = input ("How old are you?")
+color = input ("What is your favorite color?")
+print ("Hello, {name}!, You are {age} years old and your favorite color {color}")
